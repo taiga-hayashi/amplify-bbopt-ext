0c4628d75b0a4c733d6f168a497840ae8e3bdba3
Taiga Hayashi <186549021+taiga-hayashi@users.noreply.github.com>
2026-07-27 19:29:13 +0900

    refactor: simplify sampling logic and remove initial_sampling module

--- src/amplify_bbopt_ext/__init__.py
+++ src/amplify_bbopt_ext/__init__.py
@@ -1,3 +1,3 @@
-from . import embarrassingly_parallel, initial_sampling, latest_filter, utils
+from . import embarrassingly_parallel, latest_filter, utils
 
-__all__ = ["embarrassingly_parallel", "initial_sampling", "latest_filter", "utils"]
+__all__ = ["embarrassingly_parallel", "latest_filter", "utils"]
